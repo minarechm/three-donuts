036a90d6927e90163e78ed3439b5bf18944a7be5
minarechm <minarechma@gmail.com>
2023-01-01 01:10:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Perf } from "r3f-perf"
+import { OrbitControls, Text3D, Float, Center, useMatcapTexture } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
 
+
+const Scene = () => {
+  const tempArray = [...Array(100)]
+  return (
+    <>
+    <Perf position="top-left"/>
+    <OrbitControls/>
+    <Center>
+      <Float>
+        <Text3D font="./fonts/helvetiker_regular.typeface.json">
+          HELLO R3F
+          <meshNormalMaterial/>
+        </Text3D>
+      </Float>
+    </Center>
+
+    { [...Array(100)].map((value, index) => 
+    <Float>
+      <mesh key={index} position={[
+        (Math.random() -0.5) * 10,
+        (Math.random() -0.5) * 10,
+        (Math.random() -0.5) * 10,
+      ]}>
+        <torusGeometry args={ [1, 0.6, 16, 32]}/>
+        <meshNormalMaterial/>
+      </mesh>
+    </Float>
+    )}
+    
+    </>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Canvas camera={{
+        position: [10, 15, 0],
+      }}>
+        <Scene/>
+      </Canvas>
+    </>
+  )
 }
 
 export default App;
